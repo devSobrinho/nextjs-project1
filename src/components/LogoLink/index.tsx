835fe049350370d504c6f09960843107d9260b7d
Daniel Sobrinho <dev.sobrinho@gmail.com>
2022-01-24 19:01:48 -0300

    <LogoLink /> Migration to TypeScript

--- src/components/LogoLink/index.tsx
+++ src/components/LogoLink/index.tsx
@@ -3,7 +3,13 @@ import * as Styled from './styles';
 import { Heading } from '../Heading';
 import Link from 'next/link';
 
-export const LogoLink = ({ text, srcImg = '', link }) => {
+export type LogoLinkProps = {
+  text: string;
+  srcImg?: string;
+  link: string;
+};
+
+export const LogoLink = ({ text, srcImg = '', link }: LogoLinkProps) => {
   const nextLink = link.match(/^\//) ? true : false;
 
   if (nextLink) {
@@ -27,10 +33,4 @@ export const LogoLink = ({ text, srcImg = '', link }) => {
       </Styled.Container>
     </Heading>
   );
-};
-
-LogoLink.propTypes = {
-  text: P.string.isRequired,
-  srcImg: P.string,
-  link: P.string.isRequired,
 };
