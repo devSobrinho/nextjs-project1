6f6da76ae8e68ae61258f0ec65f39a7ba3f82df5
Daniel Sobrinho <dev.sobrinho@gmail.com>
2022-01-24 19:38:46 -0300

    <GridContent /> Migration to TypeScript

--- src/components/GridContent/stories.tsx
+++ src/components/GridContent/stories.tsx
@@ -1,14 +1,15 @@
-import { GridContent } from '.';
+import { Meta, Story } from '@storybook/react/types-6-0';
 
+import { GridContent, GridContentProps } from '.';
 import mock from './mock';
 
 export default {
   title: 'GridContent',
   component: GridContent,
   args: mock,
-};
+} as Meta;
 
-export const Template = (args) => {
+export const Template: Story<GridContentProps> = (args) => {
   return (
     <div>
       <GridContent {...args} />
