0946f0a429cb6a146e6de01cd1a3c2622cc7cf71
Daniel Sobrinho <dev.sobrinho@gmail.com>
2022-01-24 21:57:21 -0300

    Added Husky and Lint-staged

--- src/api/map-menu.ts
+++ src/api/map-menu.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { MenuLinkProps } from '../components/MenuLink';
 import { PageData } from '../templates/Home';
 
--- src/api/map-sections.ts
+++ src/api/map-sections.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { GridContentProps } from '../components/GridContent';
 import { GridImageElementProps, GridImageProps } from '../components/GridImage';
